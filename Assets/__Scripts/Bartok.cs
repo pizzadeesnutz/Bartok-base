fc91e21c58a06e243bccde4e7acc92d35da20118
pizzadeesnutz <arcadiomj@yahoo.com>
2016-04-10 16:21:56 -0400

    Errors galore
    Even after tracing the code to see which functions were causing the
    errors I still can't figure out what is wrong. That is, I don't know how
    a negative number is being passed as an index...
    
    ArgumentOutOfRangeException: Cannot be negative.
    Parameter name: length
    System.String.Substring (Int32 startIndex, Int32
    length) (at
    /Users/builduser/buildslave/mono-runtime-and-classlibs/build/mcs/class/corlib/System/String.cs:348)
    PT_XMLReader.ParseTag
    (System.String eS, .PT_XMLHashtable eH) (at
    Assets/__Scripts/PT_XMLReader.cs:146)
    PT_XMLReader.Parse (System.String
    eS, .PT_XMLHashtable eH) (at
    Assets/__Scripts/PT_XMLReader.cs:55)
    PT_XMLReader.ParseTag
    (System.String eS, .PT_XMLHashtable eH) (at
    Assets/__Scripts/PT_XMLReader.cs:177)
    PT_XMLReader.Parse (System.String
    eS, .PT_XMLHashtable eH) (at
    Assets/__Scripts/PT_XMLReader.cs:55)
    PT_XMLReader.Parse (System.String
    eS) (at Assets/__Scripts/PT_XMLReader.cs:48)
    BartokLayout.ReadLayout
    (System.String xmlText) (at
    Assets/__Scripts/BartokLayout.cs:33)
    Bartok.Start () (at
    Assets/__Scripts/Bartok.cs:28)

--- Assets/__Scripts/Bartok.cs
+++ Assets/__Scripts/Bartok.cs
@@ -13,6 +13,9 @@ public class Bartok : MonoBehaviour {
 	public List<CardBartok> drawPile;
 	public List<CardBartok> discardPile;
 
+	public BartokLayout layout;
+	public Transform layoutAnchor;
+
 	void Awake(){
 		S = this;
 	}//end of Awake()
@@ -21,7 +24,16 @@ public class Bartok : MonoBehaviour {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
 		Deck.Shuffle (ref deck.cards);
+		layout = GetComponent<BartokLayout> ();
+		layout.ReadLayout (layoutXML.text);
+		drawPile = UpgradeCardsList (deck.cards);
 	}//end of Start()
+
+	List<CardBartok> UpgradeCardsList(List<Card> lCD){
+		List<CardBartok> lCB = new List<CardBartok>();
+		foreach(Card tCD in lCD) lCB.Add(tCD as CardBartok);
+		return lCB;
+	}//end of UpgradeCardsList(List<Card> lCD)
 
 	void Update () {
 	
